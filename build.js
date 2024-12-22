49ae0f18339058a5bc5bd15dc03213bc6d4b41af
Walter Melo <41174751+walmello@users.noreply.github.com>
2024-12-22 17:09:05 -0300

    improving json output

--- build.js
+++ build.js
@@ -7,8 +7,25 @@ const entries = fs.readdirSync('./src').filter(entry => {
 })
 
 entries.forEach(entry => {
+  const result = []
+
   const subentries = fs.readdirSync(`./src/${entry}`).filter(entry => {
   return !['index.json'].includes(entry)
 })
-  fs.writeFileSync(`./src/${entry}/index.json`, JSON.stringify(subentries))
+  subentries.forEach(subentry => {
+    const type = subentry.split('.')[1]
+    const aux = {
+      [subentry]: subentry
+    }
+    if(type === 'json'){
+      const content = JSON.parse(fs.readFileSync(`./src/${entry}/${subentry}`, 'utf-8'))
+      console.log(content)
+      result.push({
+        [subentry]: content
+      })
+    } else {
+      result.push(subentry)
+    }
+  })
+  fs.writeFileSync(`./src/${entry}/index.json`, JSON.stringify(result, null, 2))
 })
